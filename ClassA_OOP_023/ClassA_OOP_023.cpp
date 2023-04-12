0d06ef0c5a0ba0e54380729da2bb9abf143df906
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-04-12 14:38:05 +0700

    untuk menginput data yang di publick

--- ClassA_OOP_023/ClassA_OOP_023.cpp
+++ ClassA_OOP_023/ClassA_OOP_023.cpp
@@ -6,4 +6,11 @@ class Mahasiswa {
 private:
 	int nim;
 	string nama;
+public:
+	void inputdata() {
+		cout << "Masukkan NIM : ";
+		cin >> nim;
+		cout << "Masukkan Nama : ";
+		cin >> nama;
+	}
 };
